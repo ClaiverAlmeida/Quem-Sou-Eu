3124f48cde6f81b5b80ab5ae32785c562e88323b
Elivelton Gama <elivelton.gama@ritarh.com.br>
2022-11-04 21:50:18 -0300

    ls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'main', component: MainComponent},
+  { path: 'main', component: MainComponent },
   // { path: '', redirectTo: 'main', pathMatch: 'full' },
   // {
   //   path: '',
